12a3d2de287b901aed5cdbf7929108e7b2b3351b
DaimoB@gmail.com <DaimoB@gmail.com@cfd91186-05d3-2cb8-cb1a-3f77027861b0>
2012-05-17 05:58:01 +0000

    changed packaegs

---
+++ src/system/helper/SimpleKey.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package system.helper;
+
+/**
+ *
+ * @author Steven
+ */
+public interface SimpleKey
+{
+	public abstract String getId();
+}
