807bdd09703d4f2fdf0ed4567c053182d4e4f460
DaimoB@gmail.com <DaimoB@gmail.com@cfd91186-05d3-2cb8-cb1a-3f77027861b0>
2012-08-24 16:31:55 +0000

    Fixed DB Bugs

--- src/system/individuals/Therapist.java
+++ src/system/individuals/Therapist.java
@@ -1,6 +1,7 @@
 
 package system.individuals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -16,6 +17,7 @@ public class Therapist extends UserAccount {
     public Therapist()
     {
         super();
+        marks = new ArrayList<Mark>();
     }
     
     @OneToMany(targetEntity=Mark.class,
@@ -30,8 +32,15 @@ public class Therapist extends UserAccount {
 
 	public Therapist(String name, String username, String emailAddress) {
 		super(name, username, emailAddress);
+		marks = new ArrayList<Mark>();
 	}
-		public void setMarks(List<Mark> marks) {
+	
+	public void setMarks(List<Mark> marks) {
 		this.marks = marks;
+	}
+		
+	public void addMark(Mark mark)
+	{
+		marks.add(mark);
 	}
 }
