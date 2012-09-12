5b2309b3c62ee084061611f0d0a97e5dc8d92d86
daimob@gmail.com <daimob@gmail.com@cfd91186-05d3-2cb8-cb1a-3f77027861b0>
2012-09-12 14:08:17 +0000

    Fixed HUGE mess up with last commit

--- src/viewController/Controller.java
+++ src/viewController/Controller.java
@@ -1,17 +1,56 @@
 package viewController;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dialog.ModalityType;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.MissingResourceException;
 
-//testing
-/**
-* @author Damian
-* 
-*/
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.hibernate.exception.ConstraintViolationException;
+
+import system.helper.Helper;
+import system.helper.MessageFade;
+import system.individuals.Child;
+import system.individuals.Guardian;
+import system.marking.Mark;
+import system.marking.Objective;
+import system.marking.Step;
+import system.model.ESDMModel;
+import system.model.Room;
+import system.sessions.Day;
+import system.sessions.Setting;
+
+
 public class Controller extends JFrame {
 
-	/**
-	 * 
-	 */
+
 	private static final String adminAccess = "a";
 	private static final String therapistAccess = "n";
 	private static final String guardianAccess = "g";
@@ -1197,7 +1236,6 @@ public class Controller extends JFrame {
 				roomView.refreshView();
 			}
 		});
-		
 		
 		
 		tabbedPane.addChangeListener(new ChangeListener() {
